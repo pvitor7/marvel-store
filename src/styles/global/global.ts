dd5eda5eb6275d3cffc846bcd850bbaa1e91d00e
paulovitor <pvitor7@hotmail.com>
2023-03-22 17:52:08 -0300

    chore: add global colors

--- src/styles/global/global.ts
+++ src/styles/global/global.ts
@@ -4,12 +4,14 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background: teal;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-family: inherit, sans-serif;
   }
 
   :root{
     --color-white-fixed: #FFFFFF;
+    --color-back-fixed: #000000;
+    --color-gray-1: #dddddd;
+    --color-green1: #5cb85c;
     }
 `
 export default GlobalStyle
