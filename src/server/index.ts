f6af143bf6107e259798f257d5c40be611222907
paulovitor <pvitor7@hotmail.com>
2023-03-22 17:48:05 -0300

    fix: edit headers params

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,6 +8,9 @@ const private_key = "ba226b1c9f8b9a563d0ed9195d339730f299c352";
 const TIMESTAMP = new Date().getTime();
 const HASH = md5(`${TIMESTAMP}${private_key}${public_key}`).toString();
 
+console.log("TIMESTAMP: ", TIMESTAMP)
+console.log("hash: ", HASH)
+
 const marvelApi = axios.create({
   baseURL: "https://gateway.marvel.com/v1/public/",
   params: {
