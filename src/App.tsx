d5ed94fe9fa3ba00d7019750ed3db841e343399e
paulovitor <pvitor7@hotmail.com>
2023-03-22 00:25:10 -0300

    chore: included router-dom@5 and config base

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import GlobalStyle from './styles/global/global';
-import Home from './pages/Home';
+import Routes from './pages';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <GlobalStyle/>
-    <Home/>
+      <GlobalStyle/>
+      <Routes/>
     </>
   )
 }
