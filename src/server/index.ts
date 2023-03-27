64bacb50c8a3a6da79c84c51a8f79d8d48b84d77
paulovitor <pvitor7@hotmail.com>
2023-03-27 03:08:01 -0300

    chore: remove consoles

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,17 +2,13 @@ import axios from "axios";
 import md5 from "crypto-js/md5";
 
 
-const public_key =  //"384f1411759c52b1c35e75f153a36d3a";
-"a93afdf852ebe2ee61d4259849044735" ;
+const public_key =  "a93afdf852ebe2ee61d4259849044735" ;
 
-const private_key = //"949304270a0c1d42334fa2a6834695e96ada81f1";
-"9ec5c5e4b7eca4a2e352455d1001595db9511a64";
+const private_key = "9ec5c5e4b7eca4a2e352455d1001595db9511a64";
 
 const TIMESTAMP = new Date().getTime();
 const HASH = md5(`${TIMESTAMP}${private_key}${public_key}`).toString();
 
-console.log("TIMESTAMP: ", TIMESTAMP)
-console.log("hash: ", HASH)
 
 const marvelApi = axios.create({
   baseURL: "https://gateway.marvel.com/v1/public/",
